fbe70e07a55bfb57d168315c66f41739c62e66b9
IanSkelskey <iskelskey@biblio.org>
2024-07-16 12:53:35 -0400

    feat: Customize default project with Bibliomation content

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-test' title`, () => {
+  it(`should have the 'Bibliomation, Inc.' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-test');
+    expect(app.title).toEqual('Bibliomation, Inc.');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-test');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Bibliomation, Inc.');
   });
 });
